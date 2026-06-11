02e06b10849ba35b89b4844a09fb4fdf12d6586f
John Kingoo <kingoojohn27@gmail.com>
2026-06-11 22:51:53 +0300

    Fix Book and Coffee classes to pass all tests

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,28 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self._size = None
+        self._price = price
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+    def tip(self):
+        self._price += 1
+        print("This coffee is great, here’s a tip!")
